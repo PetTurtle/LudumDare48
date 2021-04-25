6f30db5836f724edb943a7ec97f74d2d08546abc
Illya Ryhan <ryhani@protonmail.com>
2021-04-25 13:20:44 -0500

    Updated Tileset Code

--- LudumDare48/Map/Map.gd
+++ LudumDare48/Map/Map.gd
@@ -1,6 +1,21 @@
 extends Node2D
 
+var sand = 0
+var sand_ore = 1
+var stone = 2
+var stone_ore = 3
+var hard_stone = 4
+var hard_stone_ore = 5
+
+var sand_bg = 0
+var stone_bg = 1
+var hand_stone_bg = 2
+
+var can_mine_stone := false
+var can_mine_hard_stone := false
+
 onready var terrain_map: TileMap = $TerrainMap
+onready var back_map: TileMap = $BackMap
 
 
 func _init():
@@ -10,18 +25,26 @@ func _init():
 func mine_tile(point: Vector2) -> void:
 	var map_point := terrain_map.world_to_map(point)
 	var id = terrain_map.get_cellv(map_point)
-	if id == 3 or id == 10:
+	if (id == stone and not can_mine_stone) or (id == hard_stone and not can_mine_hard_stone):
 		return
 	
-	if id == 4:
+	if id == sand_ore:
 		G.set_money(G.money + 2)
-	elif id == 5:
+	elif id == stone_ore:
 		G.set_money(G.money + 4)
-	elif id == 10:
+	elif id == hard_stone_ore:
 		G.set_money(G.money + 8)
 	
 	terrain_map.set_cellv(map_point, -1)
 	terrain_map.update_bitmask_area(map_point)
+	
+	if (id == sand or id == sand_ore):
+		back_map.set_cellv(map_point, sand_bg)
+	if (id == stone or id == stone_ore):
+		back_map.set_cellv(map_point, stone_bg)
+	if (id == hard_stone or id == hard_stone_ore):
+		back_map.set_cellv(map_point, hand_stone_bg)
+	
 
 
 func has_tile(point: Vector2) -> bool:
@@ -54,16 +77,24 @@ func _explode(x: int, y: int, stength: int) -> void:
 		return
 	
 	var id = terrain_map.get_cell(x, y)
-	if id == 4:
+	if id == sand_ore:
 		G.set_money(G.money + 2)
-	elif id == 5:
+	elif id == stone_ore:
 		G.set_money(G.money + 4)
-	elif id == 10:
+	elif id == hard_stone_ore:
 		G.set_money(G.money + 8)
 		
-	var point = Vector2(x, y)
-	terrain_map.set_cellv(point, -1)
-	terrain_map.update_bitmask_area(point)
+	var map_point = Vector2(x, y)
+	terrain_map.set_cellv(map_point, -1)
+	terrain_map.update_bitmask_area(map_point)
+	
+	if (id == sand or id == sand_ore):
+		back_map.set_cellv(map_point, sand_bg)
+	if (id == stone or id == stone_ore):
+		back_map.set_cellv(map_point, stone_bg)
+	if (id == hard_stone or id == hard_stone_ore):
+		back_map.set_cellv(map_point, hand_stone_bg)
+	
 	_explode(x + 1, y, stength - 1)
 	_explode(x - 1, y, stength - 1)
 	_explode(x, y + 1, stength - 1)
